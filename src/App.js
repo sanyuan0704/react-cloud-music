5612717049619a2c2d097faeda1d8ba545abf244
sanyuan <494130947@qq.com>
2019-08-29 22:28:31 +0800

    初始化完成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { IconStyle } from './assets/iconfont/iconfont';
+import { GlobalStyle } from './style';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      <i className="iconfont">&#xe62b;</i>
     </div>
   );
 }
