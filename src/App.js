347b6fd31dc3e079bdf0aa3cc59e198b12780c57
sanyuan <494130947@qq.com>
2019-08-30 10:29:08 +0800

    完成项目基础搭建

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { IconStyle } from './assets/iconfont/iconfont';
-import { GlobalStyle } from './style';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { GlobalStyle } from  './style'
+import { renderRoutes } from 'react-router-config'
+import { IconStyle } from './assets/iconfont/iconfont'
+import store from './store/index'
+import routes from './routes/index.js'
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      <i className="iconfont">&#xe62b;</i>
-    </div>
-  );
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        { renderRoutes(routes) }
+      </HashRouter>
+    </Provider>
+  )
 }
 
 export default App;
