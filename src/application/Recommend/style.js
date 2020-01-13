4a1f9a06d4507fbfad1c01f82a72a888d949468d
sanyuan <494130947@qq.com>
2020-01-13 14:24:07 +0800

    解决推荐歌单下拉后顶部出现多于红色的问题

--- src/application/Recommend/style.js
+++ src/application/Recommend/style.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Content = styled.div`
     position: fixed;
-    top: 90px;
+    top: 94px;
     left: 0;
     bottom: ${props => props.play > 0?"60px": 0};
     width: 100%;
