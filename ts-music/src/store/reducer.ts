e2c3a9f25c4f092c315b3757abe0fccb18fbc3ff
yangxingyuan <yangxingyuan@bytedance.com>
2020-07-29 18:57:15 +0800

    feat: init project

---
+++ ts-music/src/store/reducer.ts
@@ -0,0 +1,20 @@
+import { combineReducers } from "redux-immutable";
+// import { reducer as recommendReducer } from "../application/Recommend/store/index";
+// import { reducer as singersReducer } from "../application/Singers/store/index";
+// import { reducer as rankReducer } from "../application/Rank/store/index";
+// import { reducer as albumReducer } from "../application/Album/store/index";
+// import { reducer as singerInfoReducer } from "../application/Singer/store/index";
+// import { reducer as playerReducer } from "../application/Player/store/index";
+// import { reducer as searchReducer } from "../application/Search/store/index";
+// import { reducer as userReducer } from "../application/User/Login/store/index";
+
+export default combineReducers({
+  // recommend: recommendReducer,
+  // singers: singersReducer,
+  // rank: rankReducer,
+  // album: albumReducer,
+  // singerInfo: singerInfoReducer,
+  // player: playerReducer,
+  // search: searchReducer,
+  // user: userReducer
+});
